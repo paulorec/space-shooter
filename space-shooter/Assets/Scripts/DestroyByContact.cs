6f92269d5c8919c54f5ccd350b24f9fc007add3f
Paulo Amorim <paulorec@gmail.com>
2017-03-18 15:08:11 -0300

    :sparkles: first asteroide

---
+++ space-shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary")
+			return;
+		
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ space-shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	void Start() {
+		
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+
+	}
+}
