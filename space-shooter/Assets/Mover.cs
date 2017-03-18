e2faf5892c52a3b5142d9b2dc23607f8dcf652fb
Paulo Amorim <paulorec@gmail.com>
2017-03-18 13:37:28 -0300

    :sparkles: lasers!

---
+++ space-shooter/Assets/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		rb.velocity = transform.forward * speed;
+
+	}
+}
---
+++ space-shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+
+	public float xMin, xMax, zMin, zMax;
+
+}
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	void FixedUpdate () {
+		
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x,boundary.xMin,boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin,boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
