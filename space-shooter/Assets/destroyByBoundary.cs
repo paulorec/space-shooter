bc9da9bc6c5cdaaab782d20ccf81e854c47a2044
Paulo Amorim <paulorec@gmail.com>
2017-03-18 14:05:59 -0300

    :sparkles: lasers!

---
+++ space-shooter/Assets/destroyByBoundary.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyByBoundary : MonoBehaviour {
+
+	void OnTriggerExit(Collider collider) {
+		Destroy (collider.gameObject);
+	}
+}
