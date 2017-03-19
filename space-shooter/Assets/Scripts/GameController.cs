7cbe874f98439a35fd55f23389e8845ef4bd4cb4
Paulo Amorim <paulorec@gmail.com>
2017-03-18 23:49:57 -0300

    :sparkles: spawning hazards

---
+++ space-shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	public int wavesInterval;
+
+	void Start() {
+				
+		StartCoroutine ( SpawnWaves ());
+
+	}
+
+	IEnumerator SpawnWaves() {
+		
+		while(true) {
+			
+			Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+
+			Quaternion spawnRotation = Quaternion.identity;
+
+			Instantiate (hazard, spawnPosition, spawnRotation);
+
+			yield return new WaitForSeconds(wavesInterval);
+		}
+		
+
+	}
+}
