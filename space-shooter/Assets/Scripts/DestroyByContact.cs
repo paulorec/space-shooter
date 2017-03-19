6fe554287ee7065788f89c67c411500f56ce5ae6
Paulo Amorim <paulorec@gmail.com>
2017-03-18 22:49:37 -0300

    :sparkles: explosions

--- space-shooter/Assets/Scripts/DestroyByContact.cs
+++ space-shooter/Assets/Scripts/DestroyByContact.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary")
 			return;
-		
+	
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
